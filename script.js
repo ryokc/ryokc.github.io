19fcdd3a37a8427f5f1a3f90f8f85b83e2209441
Lance Lui <lancelui02@gmail.com>
2024-03-28 13:16:17 -0500

    link rules

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const options = {
     animationSelector: '[class*="transition-"]',
     containers: ["#swup"],
     cache: true,
-    linkSelector:
-      'a[href^="' +
-      window.location.origin +
-      '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup])',
-    skipPopStateHandling: function(event) {
-      if (event.state && event.state.source == "swup") {
-        return false;
-      }
-      return true;
-    }
+    linkSelector: 'a[href]'
+    // linkSelector:
+    //   'a[href^="' +
+    //   window.location.origin +
+    //   '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup])',
+    // skipPopStateHandling: function(event) {
+    //   if (event.state && event.state.source == "swup") {
+    //     return false;
+    //   }
+    //   return true;
+    // }
   };
 
 const swup = new Swup(options);
